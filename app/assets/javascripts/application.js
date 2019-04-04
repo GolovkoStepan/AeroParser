242e893f009d0daf6cce1b53deceba97e68d0b17
stepan <stepagolovko1993@gmail.com>
2019-04-04 23:51:47 +0500

    Search was created via JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,19 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
+
+// Search JS code
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('input[type="text"]').keyup(function () {
+            let searchText = $(this).val();
+
+            $('ul > li').each(function () {
+                let currentLiText = $(this).text().toLowerCase(),
+                    showCurrentLi = currentLiText.indexOf(searchText.toLowerCase()) !== -1;
+
+                $(this).toggle(showCurrentLi);
+            });
+        });
+    });
+});
